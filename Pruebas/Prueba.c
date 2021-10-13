56cd44ecd3f4d6adebe864b187777f3d73f8ce04
Airam123456 <perseo_39@hotmail.es>
2021-10-13 08:09:32 +0200

    Actualizacion

--- Pruebas/Prueba.c
+++ Pruebas/Prueba.c
@@ -1,31 +1,82 @@
-// lecturaescritura.c
-#include <fcntl.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>  // funcion strlen
 #include <unistd.h>
 
-int main(){
-  int fd, fd2, numbytes;
-  char buffer[10];
-  char saludo[]="Un saludo!!!";
+void main ()
+{	
+	int fd1[2],fd2[2];
+	char buffer[80];
+	pid_t pid, pid2;
 
-//// Escritura
-  fd=open ("texto.txt",1); //// fichero se abre solo para escritura
-  if (fd == -1)
-     {
-                printf("Error al abrir el fichero...\n");
-                exit (-1);
-     }
-   printf("Escribo el saludo .... \n");
-   write(fd, saludo, strlen(saludo));
-   close(fd);
+	pipe(fd1);
+	pipe(fd2);
+	pid = fork();
+	switch(pid)
+	{
+		case -1:
+			printf("No se ha podido crear el proceso HIJO\n");
+			exit(-1);
+		case 0:						//HIJO
+			pid2 = fork();
+			switch(pid2)
+			{
+				case -1:
+					printf("No se ha podido crear el proceso NIETO\n");
+					exit(-1);
+				case 0: 			//NIETO
+					//RECIBE DEL HIJO
+					close(fd2[1]);
+					read(fd2[0], buffer, sizeof(buffer));
+					printf("\t\tEl NIETO recibe mensaje del padre: %s\n", buffer);
+					
+					//ENVIAR HIJO
+					close(fd1[0]);
+					char saludoNieto[] = "Saludo del nieto";
+					write(fd1[1], saludoNieto, sizeof(saludoNieto));
+					printf("\t\tEl NIETO envía un mensaje al HIJO\n");
 
-//// Lectura
-  printf("Contenido del Fichero \n");
-  fd2 = open("texto.txt", 0); //// fichero se abre solo para lectura
-  while ((numbytes = read(fd2, &buffer, sizeof(char))) > 0){
-        printf("%s",buffer);
-  }
-  close(fd2);
+					exit(0);
+				default: 			//HIJO
+					//RECIBE DEL PADRE
+					close(fd1[1]);
+					read(fd1[0], buffer, sizeof(buffer));
+					printf("\tEl HIJO recibe mensaje del ABUELO: %s\n", buffer);
+					
+					//ENVIAR AL NIETO
+					char saludoHijo[] = "Saludo del padre";
+					write(fd2[1], saludoHijo, sizeof(saludoHijo));
+					printf("\tEl HIJO envía un mensaje al NIETO......\n");
+
+					wait(NULL);
+
+					//RECIBE DEL NIETO
+					close(fd1[1]);
+					read(fd1[0], buffer, sizeof(buffer));
+					printf("\tEL HIJO recibe mensaje de su hijo: %s\n", buffer);
+					
+					//ENVIAR AL ABUELO
+					char saludoHijo2[] = "Saludo del hijo";
+					close(fd2[0]);
+					write(fd2[1], saludoHijo2, sizeof(saludoHijo2));
+					printf("\tEl HIJO envia un mensaje al ABUELO......\n");
+						
+					exit(0);
+			}
+			break;
+		default: 					//ABUELO
+			//ENVIAR AL HIJO
+			close(fd1[0]);
+			char saludoAbuelo[] = "Saludo del abuelo..";
+			write(fd1[1], saludoAbuelo, sizeof(saludoAbuelo));	
+			printf("El ABUELO envía un mensaje al HIJO.......\n");
+			
+			wait(NULL);
+
+			//RECIBE DEL HIJO
+			close(fd2[1]);
+			read(fd2[0], buffer, sizeof(buffer));
+			printf("El ABUELO recibe mensaje del HIJO: %s\n", buffer);
+	}
+	exit(0);
 }
--- Tanda_2_Pipe/Actividad_8.c
+++ Tanda_2_Pipe/Actividad_8.c
@@ -2,75 +2,73 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/wait.h>
+/*
+CREAR LAS PIPES AL PRINCIPIO Y USAR SWITCH
 
+
+*/
 void main ()
 {
+    pid_t pid1, pid2;
     int fd1[2], fd2[2];
-    char buffer[80];
+    char buffer[20];
 	char saludoAbuelo[]="Saludo del abuelo..";
     char saludoPadre[]="Saludo del padre..";
     char saludoHijo[]="Saludo del hijo..";
     char saludoNieto[]="Saludo del nieto..";
-	pid_t pid1, pid2;
     pipe(fd1); // Se crea el pipe o tubería
-	pid1 = fork();
+	pid1 = fork(); //Se crea el hijo
+
     if(pid1 == -1)
     {
-        printf("No se ha podido crear el proceso hijo...\n");
-		exit (-1);
-		
+        printf("No se ha podido crear el proceso hijo");
+        exit(0);
+    }
+
+    if(pid1 == 0) //Porceso Hijo
+    {
+        pipe(fd2); // Se crea el pipe o tubería
+        pid2 = fork; // Se crea el nieto
+    }
+
+    if(pid2 == 0) // Proceso nieto
+    {
+        close(fd2[1]);
+        read(fd2[0], buffer, sizeof(buffer));
+        printf("\t\tEl NIETO recibe mensaje del padre: %s\n", buffer);
+        printf("\t\tEl NIETO envia un mensaje al HIJO...\n");
+        close(fd2[0]);
+        write(fd2[1], saludoNieto, sizeof(saludoNieto));
     }
     else
     {
-        if (pid1 == 0)
+        if(pid1 == 0) // Proceso hijo
         {
-            pipe(fd2);
+            close(fd1[1]);
+            read(fd1[0], buffer, sizeof(buffer));
+            printf("\tEl HIJO recibe mensaje del abuelo: %s\n", buffer);
+            printf("\tEl HIJO envia un mensaje al NIETO...\n");
+            close(fd2[0]);
+            write(fd2[1], saludoPadre, sizeof(saludoPadre));
+            wait(NULL);
+            close(fd2[1]);
+            read(fd2[0], buffer, sizeof(buffer));
+            printf("\tEl HIJO recibe mensaje de su hijo: %s\n", buffer);
+            printf("\tEl HIJO envia un mensaje al ABUELO...\n");
+            close(fd1[0]);
+            write(fd1[1], saludoHijo, sizeof(saludoHijo));
+        }
+        else //Proceso abuelo
+        {
+            printf("El ABUELO envia mensaje al HIJO...\n");
+            close(fd1[0]);
+            write(fd1[1], saludoAbuelo, sizeof(saludoAbuelo));
+            wait(NULL);
+            close(fd1[1]);
+            read(fd1[0], buffer, sizeof(buffer));
+            printf("\tEl ABUELO recibe mensaje del HIJO: %s\n", buffer);
         }
     }
-
-    if(pid2 == 0)
-    {
-        close(fd2[1]); // Cierra el descriptor de escritura
-        read (fd2[0], buffer, sizeof(buffer)); // leo el pipe
-        printf ("\tEl NIETO recibe mensaje del padre: %s\n", buffer);
-        printf ("\tEl NIETO envia un mensaje al hijo....\n");
-        write (fd2[1], saludoPadre, sizeof(saludoPadre));  
-    }
-    else
-    {
-        
-    }
-    /*
-        {
-            close(fd1[0]); // Cierra el descriptor de lectura
-		    write (fd1[1], saludoAbuelo, sizeof(saludoAbuelo));
-		    printf ("El ABUELO envía un mensaje al HIJO....\n");
-
-        } 
-
-
-
-
-    if(pid == -1)
-    {
-        printf("No se ha podido crear el proceso hijo...\n");
-        exit (-1);
-        
-    }
-    else
-    {
-        if (pid == 0)
-        {
-            
-        }
-        else
-        {
-            close(fd2[0]); // Cierra el descriptor de lectura
-            write (fd2[1], saludoPadre, sizeof(saludoPadre));
-            printf ("\tEl HIJO envía un mensaje al NIETO....\n");
-            wait;
-        }  
-    }
-    */
+    exit(0);
 }
 
